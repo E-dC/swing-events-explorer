41cc13b9cad73e5db432e0a1bacfc3f38020fb66
EdC <e.decrecy@posteo.net>
2020-02-07 15:34:33 +0000

    Add placenames loading function, update call_geonames to make use of it, change variable and functions naming style before it's too late.

--- utils.R
+++ utils.R
@@ -1,10 +1,20 @@
 library('tidyverse')
 library('rvest')
+library('jsonlite')
 
-call.geonames <- function(location, country, continent) {
+
+build_placenames_list <- function(data_file = 'data/geonames.jsonlines'){
+  json_data <- stream_in(file(data_file))
+  placenames <- list(subset(json_data, ascii_name != '')$ascii_name)
+
+  return (placenames)
+}
+placenames <- build_placenames_list()
+
+call_geonames <- function(location, country, continent) {
   
-  do.request <- function(location, country, continent){
-    code.continent <- switch(continent,
+  do_request <- function(location, country, continent){
+    code_continent <- switch(continent,
       'europe'        = 'EU',
       'Europe'        = 'EU',
       'North America' = 'NA',
@@ -19,21 +29,21 @@ call.geonames <- function(location, country, continent) {
       'australasia'  = 'OC',
     )
 
-    query.url <- paste0('http://www.geonames.org/advanced-search.html?',
+    query_url <- paste0('http://www.geonames.org/advanced-search.html?',
                         'q=',
                         location,
                         '+',
                         country,
                         '&featureClass=P&continentCode=',
-                        code.continent)
+                        code_continent)
     
-    req <- POST(url = query.url)
+    req <- POST(url = query_url)
     response <- read_html(req)
   
     return (response)
   }
   
-  parse.response <- function(response){
+  parse_response <- function(response){
     geo <- response %>% html_node(xpath = '//span[@class = "geo"]')
     lon <- geo %>% html_node('span.longitude') %>% html_text(trim = TRUE) %>% as.double()
     lat <- geo %>% html_node('span.latitude') %>% html_text(trim = TRUE) %>% as.double()
@@ -41,14 +51,14 @@ call.geonames <- function(location, country, continent) {
     return (list('longitude' = lon, 'latitude' = lat))
   }
     
-  response <- do.request(location, country, continent)
-  result <- parse.response(response)
+  response <- do_request(location, country, continent)
+  result <- parse_response(response)
   
   if (has_element(result, NA)){
     els <- strsplit(location, split = '(,| )')[[1]]
-    # best_guess <- els[els %in% placenames[[1]]][1]
-    response <- geonames_call(location = els, country = '', code.continent = '')
-    result <- parse.response(response)
+    best_guess <- els[els %in% placenames[[1]]][1]
+    response <- geonames_call(location = els, country = '', code_continent = '')
+    result <- parse_response(response)
   }
   
   return (result)
