60ae8193f23164b04f5dbe868175eb55f504cc33
EdC <e.decrecy@posteo.net>
2020-06-22 21:33:46 +0100

    Update tests.

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,7 +1,3 @@
 library(testthat)
+devtools::test(reporter = 'summary')
 
-root_dir <- '~/dev/shiny_swing'
-
-source(file.path(root_dir, 'utils.R'))
-
-test_dir(file.path(root_dir, 'tests', 'testthat'), reporter='summary')
--- tests/testthat/test_utils.R
+++ tests/testthat/test_utils.R
@@ -1,11 +1,11 @@
 testthat::context("Geonames calls")
 testthat::test_that("Paris is at the correct latitude and longitude", {
-  
-  # placenames_file <- file.path(devtools::package_file(), 'data', 'geonames.jsonlines')
-  # testthat::skip_if(! file.exists(placenames_file), paste(placenames_file, 'not found, skipping test'))
-  # placenames <- build_placenames_list(placenames_file)
 
-  lat_lon <- list(latitude = 48.85341, longitude = 2.3488)
+  lat_lon <- list(latitude = 48.85341,
+                  longitude = 2.3488,
+                  country = 'France',
+                  continent = 'Europe',
+                  location = 'Paris')
   
   testthat::expect_mapequal(call_geonames('Paris', 'France', 'Europe'), lat_lon)
   testthat::expect_mapequal(call_geonames('Paris', 'France', 'Eurezfez'), lat_lon)
@@ -25,36 +25,6 @@ testthat::context("Cleaning functions")
     "Moe Sakan & Vincenzo Fesi - Anna-Maria Bernhard Ralf Bernhard",
     "Arnas Razgunas & Egle Nemickaite (Lithuania) Abeth Farag & Martynas Stonys (Portugal/Lithuania)",
     "Nick Williams, Laura Keat,Jeremy Otth, Tise Chao, Kelly Arsenault & Mickey Fortenasce Matt Mitchell, Jennifer Reed, Jennifer Barnett, Shannon Butler & Bill Butler Annabelle Hale & Nial Bruce Tabitha Robinson & Joseph Robinson Andrew MuÃ±oz Amanda Pincock & Zach Lockett-Streiff")
-
-  set_split_test_data <- list(
-    c('Meghan Gilmore',
-      'Yana Sanamyantz',
-      'Jon Tigert'),                                                       
-    c('Shani Brown and Mel Calanglang',
-      'Anni Skoglund and Gasper Hrovat'),
-    c('Dawa Jung and Youngdon Kwon from Seoul',
-      'Niclas Palmér & Sandra Thörn from Gothenburg',
-      'Anne Helene  Cavasa and Bernard Cavasa from Toulouse'),
-    c('Noni Healy',
-      'Sibby Dillon', 
-      'Shieva Norusian',
-      'Rebecca Maria'),
-    c('Joel Plys & Irina Amzashvili',
-      'Andreas Olsson & Olga Marina',
-      'Phil & Aude',
-      'Cathy & Gilbert'),
-    c('Moe Sakan & Vincenzo Fesi',
-      'Anna-Maria Bernhard Ralf Bernhard'),
-    c('Arnas Razgunas & Egle Nemickaite (Lithuania) Abeth Farag & Martynas Stonys (Portugal/Lithuania)'),
-    c('Nick Williams',
-      'Laura Keat',
-      'Jeremy Otth',
-      'Tise Chao',
-      'Kelly Arsenault & Mickey Fortenasce Matt Mitchell',
-      'Jennifer Reed',
-      'Jennifer Barnett',
-      'Shannon Butler & Bill Butler Annabelle Hale & Nial Bruce Tabitha Robinson & Joseph Robinson Andrew MuÃ±oz Amanda Pincock & Zach Lockett-Streiff')
-  )
   
   fully_split_test_data <- list(
     list(c('Meghan Gilmore'),
@@ -74,7 +44,8 @@ testthat::context("Cleaning functions")
          c('Phil', 'Aude'),
          c('Cathy', 'Gilbert')),
     list(c('Moe Sakan', 'Vincenzo Fesi'),
-         c('Anna-Maria Bernhard', 'Ralf Bernhard')),
+         c('Anna-Maria Bernhard'),
+         c('Ralf Bernhard')), # Ideally AM & R Bernhard could be matched
     list(c('Arnas Razgunas', 'Egle Nemickaite'),
          c('Abeth Farag', 'Martynas Stonys')),
     list(c('Nick Williams'),
@@ -91,27 +62,15 @@ testthat::context("Cleaning functions")
          c('Andrew MuÃ±oz'),
          c('Amanda Pincock', 'Zach Lockett-Streiff'))
   )
-  
-  
-  
-testthat::test_that("Extract likely sets from a teachers name string.", {
-  test_data <- setNames(set_split_test_data, nm = raw_test_data)
-  
-  for (n in names(test_data)){
-    testthat::expect_equal(split_on_set_separators(n), test_data[[n]])
-  }
-    testthat::expect_equal(split_on_set_separators(''), '')
-  
-})
-
 
 testthat::test_that("Extracting teachers names from string", {
   test_data <- setNames(fully_split_test_data, nm = raw_test_data)
   
   for (n in names(test_data)){
-    testthat::expect_equal(extract_clean_sets(n), test_data[[n]])
+    o <- process_teacher_description(n) %>% find_sets(capturing_patterns)
+    testthat::expect_equal(o, test_data[[n]])
   }
-  testthat::expect_equal(extract_clean_sets(''), NA)
+  testthat::expect_equal(find_sets('', capturing_patterns), NA)
   
 })
 
